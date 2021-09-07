db9b15949b649342340ef245a1ed24e4cbced79c
AlexeyFL <alexeyfl563@gmail.com>
2021-09-07 16:09:37 +0300

    feat: add menu

--- src/app/shop/components/menu/menu.component.ts
+++ src/app/shop/components/menu/menu.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { MainCategory } from '../../models/response-models';
+import { DatabaseService } from '../../services/database.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+  categories: MainCategory[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private databaseService: DatabaseService) {
+    this.categories = [];
   }
 
+  ngOnInit() {
+    this.databaseService.getCategories();
+    this.databaseService.categories$
+      .pipe(map((data: MainCategory[]) => data))
+      .subscribe((data) => {
+        this.categories = data;
+      });
+  }
 }
